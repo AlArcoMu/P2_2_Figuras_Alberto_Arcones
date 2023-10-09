83325404257a7941ffa70c05a8ab0b6a424deb92
AlArcoMu <arconescaballero74@gmail.com>
2023-10-09 09:15:42 +0200

    3 commit(se me olvidó poner la transparencia)

--- Color.cs
+++ Color.cs
@@ -1,14 +1,16 @@
-
+namespace P2_2_Figuras;
 public struct Color
 {
     public int Red { get; set; }
     public int Green { get; set; }
     public int Blue { get; set; }
+    public int Alpha { get; set; } // Componente de transparencia
 
-    public Color(int red, int green, int blue)
+    public Color(int red, int green, int blue, int alpha)
     {
         Red = red;
         Green = green;
         Blue = blue;
+        Alpha = alpha;
     }
 }
--- P2_2_Figuras.cs
+++ P2_2_Figuras.cs
@@ -10,6 +10,32 @@ public class P2_2_Figuras
 {
   static void Main(string[] args)
   {
+    Color Rojo = new Color(255, 0, 0, 0);
+
+    Rectangulo rectangulo = new Rectangulo(Rojo, 8.0, 2.0);
+    Circulo circulo = new Circulo(Rojo, 4.3);
+    TrianguloEquilatero triangulo = new TrianguloEquilatero(Rojo, 6.8);
+    Console.WriteLine(rectangulo.ToString());
+    Console.WriteLine($"Área del rectángulo: {rectangulo.GetArea()}");
+
+    Console.WriteLine(circulo.ToString());
+    Console.WriteLine($"Área del círculo: {circulo.GetArea()}");
+
+    Console.WriteLine(triangulo.ToString());
+    Console.WriteLine($"Área del triángulo equilátero: {triangulo.GetArea()}");
+
+    rectangulo.BaseRectangulo = 10.0;
+    circulo.Radio = 5.0;
+    triangulo.LongitudLado = 8.2;
+    Console.WriteLine(rectangulo.ToString());
+    Console.WriteLine($"Área del rectángulo: {rectangulo.GetArea()}");
+
+    Console.WriteLine(circulo.ToString());
+    Console.WriteLine($"Área del círculo: {circulo.GetArea()}");
+
+    Console.WriteLine(triangulo.ToString());
+    Console.WriteLine($"Área del triángulo equilátero: {triangulo.GetArea()}");
+
 
   }
 
